0590ed5215ed383d6bd3690ca94c5e8cd5471a9b
Miles Rausch <miles.rausch@carsforsale.com>
2019-06-04 15:48:22 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+// Generated on 2019-06-04 using generator-bookmarklet 1.2.0
+'use strict';
+
+var del = require('del'),
+    fs = require('fs'),
+    gulp = require('gulp'),
+    gulpConcat = require('gulp-concat'),
+    gulpJshint = require('gulp-jshint'),
+    minify = require('gulp-babel-minify'),
+    jshintStylish = require('jshint-stylish'),
+    replace = require('gulp-replace');
+
+gulp.task('scripts', ['clean'], function () {
+  return gulp.src('app/{,*/}*.js')
+    .pipe(gulpJshint())
+    .pipe(gulpJshint.reporter(jshintStylish))
+    .pipe(minify())
+    .pipe(gulpConcat('bookmarklet.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('htmls', ['clean'], function () {
+  return gulp.src('app/*.html')
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('clean', del.bind(null, 'dist'));
+
+gulp.task('default', ['clean', 'scripts', 'htmls'], function () {
+  var contents = fs.readFileSync('dist/bookmarklet.js');
+
+  return gulp.src('dist/index.html')
+    .pipe(replace('{{ bookmarklet }}', contents))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('app/{,*/}*.js', ['scripts']);
+});
