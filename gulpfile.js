e25d4c633c2b0acc9e4a7dff905ac1c56039111c
Miles Rausch <miles.rausch@carsforsale.com>
2021-03-09 21:24:17 -0600

    Remove some useless plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 const { series, parallel, src, dest } = require('gulp');
 const del = require('del');
 const gulpConcat = require('gulp-concat');
-const gulpJshint = require('gulp-jshint');
 const minify = require('gulp-babel-minify');
-const jshintStylish = require('jshint-stylish');
 
 const sourceFolder = 'app';
 const destFolder = 'dist';
@@ -20,8 +18,6 @@ function htmls(cb) {
 
 function scripts(cb) {
   return src(`${sourceFolder}/{,*/}*.js`)
-      .pipe(gulpJshint())
-      .pipe(gulpJshint.reporter(jshintStylish))
       .pipe(minify())
       .pipe(gulpConcat('bookmarklet.js'))
       .pipe(dest(destFolder));
